841dbf0cc3590f9908551283309885c1c8383998
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-06-19 19:52:15 +0200

    set background with javascript

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,23 @@
 "use strict";
 let gridItems = document.getElementsByClassName('grid-item');
 let gridTileHeight = gridItems[0].clientHeight;
+
+let cars = [
+  "rl_background01.png",
+  "rl_background02.jpg",
+  "rl_background03.jpg"
+];
+
 startFunction();
 
 function startFunction() {
   centerTileText();
+  setBackground();
+}
+
+function setBackground() {
+  let background = document.getElementById("background");
+  background.style.backgroundImage = "url('resources/images/background/rl_background01.png')";
 }
 
 
