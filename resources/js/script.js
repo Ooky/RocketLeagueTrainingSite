a3c72e21cccbf21accaf92e75bc047c40e634a2a
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-06-19 18:59:17 +0200

    centered Text

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,2 +1,15 @@
-
 "use strict";
+let gridItems = document.getElementsByClassName('grid-item');
+let gridTileHeight = gridItems[0].clientHeight;
+startFunction();
+
+function startFunction() {
+  centerTileText();
+}
+
+
+function centerTileText() {
+  for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].style.lineHeight = gridTileHeight + "px";
+  }
+}
