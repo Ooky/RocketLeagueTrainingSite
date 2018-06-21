656a51415ec340dc6813b8a60508669312b88150
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-06-21 15:58:37 +0200

    Added videos to website

--- resources/js/script.js
+++ resources/js/script.js
@@ -3,21 +3,63 @@ let gridItems = document.getElementsByClassName('grid-item');
 let gridContainerTilesChildNodes = document.getElementsByClassName("grid-container-tiles")[0].childNodes;
 let trainingTiles = [];
 let isMaximazed = false;
+let jsonObject = null;
 
-let backgrounds = [
-  "resources/images/background/rl_background01.png",
-  "resources/images/background/rl_background02.jpg",
-  "resources/images/background/rl_background03.jpg"
-];
 
 startFunction();
 
 function startFunction() {
   window.addEventListener('resize', centerTileText);
+  window.addEventListener('resize', resizeVideos);
+  generateJSONObject("resources/json/data.json");
   centerTileText();
   setBackground();
   initTrainingTiles();
   addEventListenerToGridTiles();
+  addVideosToHTML();
+}
+
+function resizeVideos() {
+  let videos = document.getElementsByTagName("video");
+  let width = getWidthOfTile(trainingTiles[0].id) + 1;
+  let height = getHeightOfTile("grid-item2") + 1;
+  for (let i = 0; i < videos.length; i++) {
+    // videos[i].width = width;
+    // videos[i].height = height;
+  }
+}
+
+function addVideosToHTML() {
+  //+1 due to rounding errors
+  let width = getWidthOfTile(trainingTiles[0].id) + 1;
+  let height = getHeightOfTile("grid-item2") + 1;
+  if (jsonObject != null) {
+    let startTile = 2;
+    for (let i = 0; i < trainingTiles.length; i++) {
+      let id = "grid-item" + (i + startTile);
+      let video = document.createElement("video");
+      video.setAttribute("width", width);
+      video.setAttribute("height", height);
+      let source = document.createElement("source");
+      source.setAttribute("src", "resources/videos/webm/" + jsonObject.webms[i].name);
+      source.setAttribute("type", "video/webm");
+      let element = document.getElementById(id);
+      video.appendChild(source);
+      element.appendChild(video);
+    }
+  } else {
+    window.setTimeout(addVideosToHTML, 100);
+  }
+}
+
+function getWidthOfTile(id) {
+  let width = document.getElementById(id).offsetWidth;
+  return width;
+}
+
+function getHeightOfTile(id) {
+  let height = document.getElementById(id).offsetHeight;
+  return height;
 }
 
 function initTrainingTiles() {
@@ -56,9 +98,14 @@ function tileInfo() {
 }
 
 function setBackground() {
-  let randomBackground = Math.floor((Math.random() * backgrounds.length));
-  let background = document.getElementById("background");
-  background.style.backgroundImage = "url(" + backgrounds[randomBackground] + ")";
+  if (jsonObject != null) {
+    let numberOfBackgrounds = jsonObject.backgrounds.length;
+    let randomBackground = Math.floor((Math.random() * numberOfBackgrounds));
+    let background = document.getElementById("background");
+    background.style.backgroundImage = "url(resources/images/background/" + jsonObject.backgrounds[randomBackground].name + ")";
+  } else {
+    window.setTimeout(setBackground, 100);
+  }
 }
 
 function centerTileText() {
@@ -66,4 +113,23 @@ function centerTileText() {
   for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].style.lineHeight = gridTileHeight + "px";
   }
+}
+
+function loadJSON(path, callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', path, true);
+  xobj.onreadystatechange = function() {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+function generateJSONObject(path) {
+  loadJSON(path, function(response) {
+    jsonObject = JSON.parse(response);
+  });
 }
